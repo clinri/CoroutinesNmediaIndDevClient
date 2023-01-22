5e6eb28699bc0a35e7348b3393a0c44504670cac
Andrei Borisov <clinri@ya.ru>
2023-01-22 17:52:55 +0300

    try to do Task 1 AuthorId

--- src/main/kotlin/dto/Post.kt
+++ src/main/kotlin/dto/Post.kt
@@ -2,10 +2,10 @@ package dto
 
 data class Post(
     val id: Long,
-    val author: String,
-    val authorAvatar: String,
+    val authorId: Long,
     val content: String,
     val published: Long,
     val likedByMe: Boolean,
-    val likes: Int = 0
+    val likes: Int = 0,
+    var attachment: Attachment? = null,
 )
--- src/main/kotlin/dto/PostWithComments.kt
+++ src/main/kotlin/dto/PostWithComments.kt
@@ -2,5 +2,36 @@ package dto
 
 data class PostWithComments(
     val post: Post,
-    val comments: List<Comment>
-)
+    val author: Author,
+    val comments: List<Pair<Comment, Author>>
+) {
+    override fun toString(): String = buildString {
+        append("=============\n")
+        append("post.id = ${post.id}\n")
+        append("post.authorId = ${post.authorId}\n")
+        append("post.authorId = ${post.authorId}\n")
+        append("    author.id = ${author.id}\n")
+        append("    author.name = ${author.name}\n")
+        append("    author.avatar = ${author.avatar}\n")
+        append("post.content = ${post.content}\n")
+        append("post.published = ${post.published}\n")
+        append("post.likedByMe = ${post.likedByMe}\n")
+        append("post.likes = ${post.likes}\n")
+        append("post.attachment = ${post.attachment}\n")
+        append("    comments:\n")
+        comments.forEach {
+            append("        ---------------\n")
+            append("        comment.id = ${it.first.id}\n")
+            append("        comment.postId = ${it.first.postId}\n")
+            append("        comment.authorId = ${it.first.authorId}\n")
+            append("            author.id = ${it.second.id}\n")
+            append("            author.name = ${it.second.name}\n")
+            append("            author.avatar = ${it.second.avatar}\n")
+            append("        comment.content = ${it.first.content}\n")
+            append("        comment.published = ${it.first.published}\n")
+            append("        comment.likedByMe = ${it.first.likedByMe}\n")
+            append("        comment.likes = ${it.first.likes}\n")
+        }
+        append("\n")
+    }
+}
